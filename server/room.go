88f3087563bf408b87d2acb867be5129c94b20dd
Emrul Kayes <ekayesorko@gmail.com>
2023-01-09 23:15:59 +0600

    Append member, Remove member and Serve function.

--- server/room.go
+++ server/room.go
@@ -4,12 +4,54 @@ import (
 	"encoding/binary"
 	"net"
 	"sync"
+	"time"
 )
 
 const arrSize = 1000
 
 type Room struct {
-	Members map[string]Member
+	endSignal chan bool
+	ticker    time.Ticker
+	Members   map[string]*Member
+}
+
+func (room *Room) AppendMember(member *Member) error {
+	if member == nil {
+		panic("whoa")
+	}
+	if _, found := room.Members[member.id]; !found {
+		room.Members[member.id] = member
+	} else {
+		panic("whoa")
+	}
+	go member.ListenMic()
+	return nil
+}
+
+func (room *Room) RemoveMember(member *Member) error {
+	delete(room.Members, member.id)
+	return nil
+}
+
+func (room *Room) Serve() error {
+	go func() {
+		for {
+			select {
+			case <-room.endSignal:
+				//close the room
+			case <-room.ticker.C:
+				req := make(map[string][]float32)
+				for _, m := range room.Members {
+					req[m.id] = m.inputChunk
+				}
+				resp := Multiplex(req)
+				for _, m := range room.Members {
+					_ = binary.Write(m.connection, binary.BigEndian, resp[m.id])
+				}
+			}
+		}
+	}()
+	return nil
 }
 
 type Member struct {
@@ -34,6 +76,7 @@ func (member *Member) ListenMic() {
 
 func (member *Member) MicChunkRegister() {
 	// read from the buffer and discard multiple chunks, keeping the latest one only
+	// call this function in every tick
 	var tempMemChunk []float32
 	for {
 		tempMemChunk = <-member.micBuffer
